c7d2bdf5e828365a272faf7186a5f5e3ab781102
wbf17739094074 <17739094074@163.com>
2019-07-10 23:38:41 +0800

    图像掩膜操作，提高清晰度课程

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #  <#Title#>
 
 第二课：https://www.bilibili.com/video/av29600072?p=2
+图像掩膜操作:https://www.bilibili.com/video/av29600072?p=3 提高图片对比度
+
---
+++ image_yanmo.hpp
@@ -0,0 +1,14 @@
+//
+//  image_yanmo.hpp
+//  myopencv
+//
+//  Created by wu baofeng on 2019/7/10.
+//  Copyright © 2019 friday. All rights reserved.
+//
+
+#ifndef image_yanmo_hpp
+#define image_yanmo_hpp
+
+#include <stdio.h>
+
+#endif /* image_yanmo_hpp */
