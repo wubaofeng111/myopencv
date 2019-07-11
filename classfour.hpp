2bdbaefd4eca55fe530a6592d6e853deb64b5793
wbf17739094074 <17739094074@163.com>
2019-07-11 23:16:31 +0800

    遍历图像元素

---
+++ classfour.hpp
@@ -0,0 +1,18 @@
+//
+//  classfour.hpp
+//  myopencv
+//
+//  Created by wu baofeng on 2019/7/11.
+//  Copyright © 2019 friday. All rights reserved.
+//
+
+#ifndef classfour_hpp
+#define classfour_hpp
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+#endif /* classfour_hpp */
